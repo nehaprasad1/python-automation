f37efec7152c16daa44b8691a3b5493a9847c32f
Neha Prasad <149758042+nehaprasad1@users.noreply.github.com>
2026-04-29 21:11:13 +0530

    Implement PDF table extraction with camelot-py
    Added functionality to extract tables from PDF using camelot-py.

--- pdf_extraction.py
+++ pdf_extraction.py
@@ -1,1 +1,12 @@
 
+#to extract table from pdf we need to import  camelot-py library 
+#use : pip install camelot-py
+# before this we need to installl two dependencies by usigng command : pip install tk and  pip insstall ghostscript
+import camelot
+
+tables = camelot.read_pdf('food.pdf', page='1')
+print(tables)
+
+tables.export('food.csv', f='csv', compress=True)
+tables[0].to_csv('foo.csv')  # to a csv file
+print(tables[0].df)  # to a df
